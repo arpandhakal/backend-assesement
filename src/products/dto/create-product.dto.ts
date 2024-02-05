d52dfb143a73114d66e34b59f9a31b788cfe06a9
Subekshya8686 <subekshyakayastha4@gmail.com>
2024-02-05 12:30:08 +0545

    Feat: create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateProductDto {
+    @IsNotEmpty()
     productName: string;
+
+    @IsNotEmpty()
     description: string;
+
+    @IsNotEmpty()
     productPrice: number;
 }
