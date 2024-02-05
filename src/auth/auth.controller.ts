c016db4d4dca93d2a12004421ffcc7c7b94da436
Phuldev Mandal <59725190+Phuldev@users.noreply.github.com>
2024-02-05 13:51:45 +0545

    Feat: login function
    -compare password, if password is wrong than throw error message if
    password is right than generate token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  //
   @Post('/signup')
   signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signUp(signUpDto);
+  }
+  //
+  @Get('/login')
+  LoginDto(@Body() logInDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.logIn(logInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Users, UsersDocument } from 'src/auth/schema/users';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +14,7 @@ export class AuthService {
     private userModel: Model<UsersDocument>,
     private jwtService: JwtService,
   ) {}
+  //
   async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
     const { user_name, user_email, password } = signUpDto;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -21,6 +23,17 @@ export class AuthService {
       user_email,
       password: hashedPassword,
     });
+    const token = this.jwtService.sign({ id: user._id });
+    return { token };
+  }
+  //
+  async logIn(loginDto: LoginDto): Promise<{ token: string }> {
+    const { user_email, password } = loginDto;
+    const user = await this.userModel
+      .findOne({ user_email })
+      .select('+password');
+    if (!user || !(await bcrypt.compare(password, user.password)))
+      throw new Error('somthing  went wrong');
     const token = this.jwtService.sign({ id: user._id });
     return { token };
   }
