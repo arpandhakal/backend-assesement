2bb5d8936262842408fa4c86e947895b4e11e8c7
Subekshya8686 <subekshyakayastha4@gmail.com>
2024-02-05 12:30:37 +0545

    Feat: product schema

--- src/products/schema/product.schema.ts
+++ src/products/schema/product.schema.ts
@@ -1,1 +1,17 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+
+export type ProductDocument = Product & Document;
+@Schema()
+export class Product {
+    @Prop()
+    productName: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    productPrice: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
